7a3b58c60169fbe95ba83d96974e700a4e2ddf7f
Emelí Pasini <emelipasini@outlook.com>
2023-10-07 17:13:23 +0200

    feat: add basic server
    build: add include and exclude to tsconfig and eslint

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT ?? 3000;
+
+app.get("/", (_req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
