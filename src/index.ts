198d83ced83f809167dabac2669a4cc560a5eac1
Emelí Pasini <emelipasini@outlook.com>
2023-10-09 18:05:17 +0200

    feat: add initial home page; add initial not found page
    build: configure views and public directories

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import express, { json } from "express";
 import "dotenv/config";
 
@@ -9,16 +10,22 @@ app.disable("x-powered-by");
 app.use(json());
 app.use(corsMiddleware);
 
+app.set("view engine", "ejs");
+app.set("views", resolve("views"));
+app.use(express.static(resolve("public")));
+
 app.get("/health", (_req, res) => {
     res.send("OK");
 });
 
 app.get("/", (_req, res) => {
-    res.send("Hello World!");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.render("index");
 });
 
 app.use((_req, res) => {
-    res.status(404).send("Not Found");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.status(404).render("not-found");
 });
 
 const PORT = process.env.PORT ?? 3000;
