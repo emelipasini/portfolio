3927007d615f7c066a6e96a0b04945f1996f1d82
Emelí Pasini <emelipasini@outlook.com>
2023-11-03 22:06:58 +0100

    refactor: add small, medium and large files with css media queries
    add toggle of menu in js when clicking a link

--- public/js/navigation.js
+++ public/js/navigation.js
@@ -1,6 +1,13 @@
+const mediaQuery = window.matchMedia("(max-width: 992px)");
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".internal-links");
 
 menuToggle.addEventListener("click", function () {
     navLinks.classList.toggle("show");
+});
+
+navLinks.addEventListener("click", function () {
+    if (mediaQuery.matches) {
+        navLinks.classList.toggle("show");
+    }
 });
