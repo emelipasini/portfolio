ad48220c414354c7f92a1115d81d1ffadb59ff5d
Emeli Pasini <emelipasini@gmail.com>
2026-03-18 12:54:31 +0100

    docs: add swagger page in /api/docs

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,14 @@ import { Router, type Request, type Response, type NextFunction } from "express"
 import infoRoutes from "./routes/info.js";
 import { AppError } from "./models/appError.js";
 
+import swaggerUi from "swagger-ui-express";
+import swagger from "../docs/swagger.json";
+
 const apiRouter = Router();
 
 apiRouter.use("/", infoRoutes);
+
+apiRouter.use("/docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
 apiRouter.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new AppError("Not found", 404));
