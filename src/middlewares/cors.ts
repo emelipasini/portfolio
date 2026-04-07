2cddfd137e0e247a897da49d9377d7df6ff6cbe8
Emeli Pasini <emelipasini@gmail.com>
2026-04-07 13:41:30 +0200

    fix: config of cors to not brake swagger styles

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -14,14 +14,17 @@ export const corsMiddleware = (req: Request, res: Response, next: NextFunction):
         res.setHeader("Access-Control-Allow-Origin", origin);
         res.setHeader("Access-Control-Allow-Methods", "GET");
         res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
-        res.setHeader("Content-Security-Policy", "default-src https:");
+        res.setHeader(
+            "Content-Security-Policy",
+            "default-src 'self' https:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'"
+        );
         res.setHeader("X-Content-Type-Options", "nosniff");
         res.setHeader("X-Frame-Options", "DENY");
         res.setHeader("Referrer-Policy", "no-referrer");
     }
 
     if (ENVIRONMENT === "development") {
-        res.header("Content-Security-Policy", "img-src 'self'");
+        res.header("Content-Security-Policy", "img-src 'self' data:; style-src 'self' 'unsafe-inline'");
     }
 
     next();
