e4477578aaf4a642c8272716ee0ffcfa6982da83
Emeli Pasini <emelipasini@gmail.com>
2026-04-09 14:49:01 +0200

    test: add thresholds to vitest config to ensure 80% coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,14 @@ export default defineConfig({
         include: ["./src/**/*.{test,spec}.ts"],
         coverage: {
             provider: "v8",
-            reporter: ["text", "json", "html"],
+            reporter: ["text", "json", "json-summary", "html"],
+            exclude: ["src/api/models/**", "public/**", "build/**", "dist/**", "node_modules/**"],
+            thresholds: {
+                lines: 80,
+                functions: 85,
+                branches: 80,
+                statements: 80,
+            },
         },
     },
 });
